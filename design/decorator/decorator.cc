de7bea78b8697e2ff2cb7179ac05a79770304556
nikola1234 <398371384@qq.com>
2017-11-23 22:45:12 +0800

    decorator done

--- design/decorator/decorator.cc
+++ design/decorator/decorator.cc
@@ -23,13 +23,50 @@ protected:
 
 private:
 };
-
-class Decorator
+// 继承Component实现Operation方法
+class Decorator : public Component
 {
 public:
-    Decorator(Component *com) 
+    Decorator(Component *com) {
+        m_com = com;
+        std::cout << "Decorator constructrue." << std::endl;
+    }
     virtual ~Decorator() {}
-    ~Decorator();
+    virtual void Operation(void) {}
 protected:
     Component* m_com;
 };
+
+class ConcreteDecorator : public Decorator
+{
+public:
+    ConcreteDecorator(Component *com) : Decorator(com){  // 派生类传输参数到基类中
+        std::cout << "ConcreteDecorator constructrue." << std::endl;
+    }
+    ~ConcreteDecorator() {};
+    void Operation(void) {
+        m_com->Operation();
+        AddOperation();
+    }
+    void AddOperation(void) {
+        std::cout << "ConcreteDecorator AddOperation." << std::endl;
+    }
+};
+/*
+本来：
+Component* com = new ConcreteComponent();
+com->Operation();
+将这个组件实现类放在装饰器中增加功能。
+ */
+ */
+int main(int argc, char const *argv[])
+{
+    Component* com = new ConcreteComponent();
+    Decorator* dec = new ConcreteDecorator(com);
+    dec->Operation();
+    delete com;
+    com = nullptr;
+    delete dec;
+    dec = nullptr;
+    return 0;
+}
